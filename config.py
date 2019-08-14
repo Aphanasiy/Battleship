67f32b09877ee60c0228c9994c19a70e9c56fd1e
Aphanasiy <M79150306109@ya.ru>
2019-08-14 19:06:06 +0300

    K

---
+++ config.py
@@ -0,0 +1,47 @@
+# User configs
+
+FIELD_FILE = "my_field.txt"
+
+"""
+It must contain field 10x10 like:
+~~~~~~~~H~
+~H~~~~~~~~
+~H~~~HHH~~
+~H~~~~~~~~
+~H~~H~~~~~
+~~~~~~~~~~
+HH~~~~~HH~
+~~~~~H~~~~
+H~~H~~~~~~
+~~~H~~~~~~
+
+HHHH x1
+HHH x2
+HH x3
+H x4
+
+No side and diagonal contact
+"""
+
+# Network config
+
+HOST = "localhost"
+PORT = 1237
+ENCODING = "utf-8"
+
+## Field display config
+
+cBASE = "~"
+cMISS = "*"
+cSHIP = "H"
+cHURT = "X"
+cDEAD = "F"
+
+## TO-SERVER config
+
+SHOT = "SHOT"
+STOP = "STOP"
+MISS = "MISS"
+HURT = "HURT"
+DEAD = "DEAD"
+
